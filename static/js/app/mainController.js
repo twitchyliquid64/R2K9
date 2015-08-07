91ff40e6c7e03ce21486515726704a9203da292f
Tom <xxx@xxx>
2015-08-07 16:45:40 +1000

    Logging system almost done.

--- static/js/app/mainController.js
+++ static/js/app/mainController.js
@@ -7,6 +7,8 @@
         var self = this;
 
         self.focus = 'logging';
+	self.logMessages = [[0,"System started."]];
+	self.startTime = Date.now();
 
         self.activate = function (element) {
             self.focus = element;
@@ -16,5 +18,9 @@
             $mdSidenav('left').toggle();
         };
 
+	self.log = function(message) {
+		self.logMessages[self.logMessages.length] = [Date.now() - self.startTime, message];
+	};
+	self.log("Main controller initialised.");
     }
 })();
