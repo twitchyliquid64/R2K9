5e2a7013d85f477cf678df2d50abcef2b1fd9526
Tom <xxx@xxx>
2015-12-09 21:47:22 +1100

    Basic parsing working. TODO assignmentt and expressions.

--- static/js/app/ddlparser.js
+++ static/js/app/ddlparser.js
@@ -101,6 +101,10 @@ function printTokens(outputTokens) {
 var AST_ROOT = "ROOT";
 var AST_DESCRIPTOR = "DESC"; //function call / object
 var AST_NUMBER_LIT = "NUMBER";
+var AST_STRING_LIT = "STRING";
+var AST_IDENTIFIER = "IDENTIFIER";
+var AST_KEY_VAL_LIT = "KEY_VAL";
+
 
 function newNode(name){
 	var ret = new Object();
@@ -120,35 +124,56 @@ function newNode(name){
 
 function run(){
 	var toks = lex();
-	console.log(recursiveParse(toks, 0));
+	console.log(recursiveParse(toks, 0).obj);
 }
 
 
 function recursiveParse( tokenSet, tokenPosition) {
 	for (var i = tokenPosition; i < tokenSet.length; i++) {
-		
 
 		if (tokenSet[i].ttype == TOKEN_IDENTIFIER) {
 			if( ((i+1) < tokenSet.length) && (tokenSet[i+1].ttype == TOKEN_OPENING_PARENTHESIS)) {//new descriptor
 				var descriptor_name = tokenSet[i].param;
 				var retNode = newNode(AST_DESCRIPTOR);
 				i += 2;
-				while ((i < tokenSet.length) && (tokenSet[i].ttype != TOKEN_CLOSING_PARENTHESIS)){
+				while ((i < tokenSet.length) && (tokenSet[i].ttype != TOKEN_CLOSING_PARENTHESIS)){//call recursive_parse until we hit closing parenthesis
 					var r = recursiveParse(tokenSet, i);
 					i = r.pos + 1;
 					retNode.add(r.nodeName, r.obj);
 				}
-				return {obj: retNode, pos: i, nodeName: descriptor_name};
+				retNode.value = descriptor_name;
+				return {obj: retNode, pos: i, nodeName: null};
+			} else { //not a function call, must be a literal
+				var retNode = newNode(AST_IDENTIFIER);
+				retNode.value = tokenSet[i].param;
+				return {obj: retNode, pos: i, nodeName: null};
 			}
 		}
 
 		else if (tokenSet[i].ttype == TOKEN_NUMBER) {
 			var retNode = newNode(AST_NUMBER_LIT);
-			return {obj: retNode, pos: i+1, nodeName: null};
+			retNode.value = tokenSet[i].param;
+			return {obj: retNode, pos: i, nodeName: null};
+		}
+
+		else if (tokenSet[i].ttype == TOKEN_STRING) {
+			var retNode = newNode(AST_STRING_LIT);
+			retNode.value = tokenSet[i].param;
+			return {obj: retNode, pos: i, nodeName: null};
+		}
+
+		else if (tokenSet[i].ttype == TOKEN_KEY) {
+			var retNode = newNode(AST_KEY_VAL_LIT);
+			var keyName = tokenSet[i].param;
+			i += 1;
+			var r = recursiveParse(tokenSet, i); //call recursive parse to parse the value
+			i = r.pos;
+			retNode.add(r.nodeName, r.obj);
+			return {obj: retNode, pos: i, nodeName: keyName};
 		}
 
 		else if (tokenSet[i].ttype == TOKEN_SEPARATOR) {
-			return {obj: null, pos: i+1, nodeName: null};
+			return {obj: null, pos: i, nodeName: null};
 		}
 
 	}
